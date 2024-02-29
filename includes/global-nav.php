3764a8590407d4e55fa8ad60390c08455312188d
200587841 <157327015+200587841@users.noreply.github.com>
2024-02-29 15:32:58 -0500

    Update global-nav.php

--- includes/global-nav.php
+++ includes/global-nav.php
@@ -7,7 +7,6 @@
 					<li><a href="/blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
 					<li><a href="/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
 					<li><a href="/green.php" title="Learn about green blocks">Green Blocks</a></li>
-					<li><a href="/purple.php" title="Learn about purple blocks">Purple Blocks</a></li>
 					<li><a href="/about.php" title="Learn more about us">About</a></li>
 				</ul>
 			</nav>
